45e19935c213a20b6567234b0cb9a0529eda683c
OscarSilvaOfficial <OscarSilvaOfficial>
2022-05-02 20:49:41 -0300

    Fix use case

--- src/core/useCases/createBillingGroupUseCase.ts
+++ src/core/useCases/createBillingGroupUseCase.ts
@@ -1,40 +1,21 @@
 import { BillingGroupRepositoryContract } from '@/adapters/contracts/billingGroupRepositoryContract';
-import { BillingRepositoryContract } from '@/adapters/contracts/billingRepositoryContract';
-import { BillingDatabaseForm } from '@/helpers/interfaces/presenters/billing';
 import { GroupBillingDatabaseForm } from '@/helpers/interfaces/presenters/groupBilling';
-import { ICreateBillingData } from '@/helpers/interfaces/useCases/createBillingData';
 import { ICreateBillingGroupData } from '@/helpers/interfaces/useCases/createBillingGroupData';
 import { Billing } from '../entities/Billing';
 import { BillingGrouping } from '../entities/BillingGrouping';
 
-export class createBillingGroupUseCase {
+export class CreateBillingGroupUseCase {
   constructor(
-    private billingRepository: BillingRepositoryContract<BillingDatabaseForm>,
     private billingGroupRepository: BillingGroupRepositoryContract<GroupBillingDatabaseForm>,
   ) {}
-
-  private async createBillingsOfGroup(
-    billingsData: ICreateBillingData[],
-  ): Promise<Billing[]> {
-    const response = billingsData.map(async (billingData) => {
-      const billing = new Billing(billingData);
-      const createdBilling = await this.billingRepository.create(billing);
-      return new Billing(createdBilling);
-    });
-    return await Promise.all(response);
-  }
 
   private async createBillingGroupWithBillings(
     billingGroupingData: ICreateBillingGroupData,
   ): Promise<BillingGrouping> {
-    const billings = await this.createBillingsOfGroup(
-      billingGroupingData.billings,
-    );
-
     const billingGrouping = new BillingGrouping({
       name: billingGroupingData.name,
       description: billingGroupingData.description,
-      billings: billings,
+      billings: billingGroupingData.billings.map(billing => new Billing(billing)),
     });
 
     const createdBillingGrouping = await this.billingGroupRepository.create(
@@ -67,9 +48,8 @@ export class createBillingGroupUseCase {
   async execute(
     billingGroupingData: ICreateBillingGroupData,
   ): Promise<BillingGrouping> {
-    if (billingGroupingData.billings.length > 0)
-      return await this.createBillingGroupWithBillings(billingGroupingData);
-
-    return await this.createBillingGroupWithNoBillings(billingGroupingData);
+    return billingGroupingData.billings ?
+      await this.createBillingGroupWithBillings(billingGroupingData) : 
+      await this.createBillingGroupWithNoBillings(billingGroupingData)
   }
 }
