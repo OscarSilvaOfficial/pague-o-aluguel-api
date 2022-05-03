214a2014909eb510fdd9ac309383e4cf9d83488d
OscarSilvaOfficial <OscarSilvaOfficial>
2022-05-02 22:28:56 -0300

    Ajuste no repositório para pegar todos os BillingGrouping

--- src/adapters/contracts/billingGroupRepositoryContract.ts
+++ src/adapters/contracts/billingGroupRepositoryContract.ts
@@ -1,5 +1,6 @@
 import { BillingGrouping } from '@/core/entities/BillingGrouping';
 
 export interface BillingGroupRepositoryContract<T> {
+  all(): Promise<T[]>;
   create(billingGrouping: BillingGrouping): Promise<T>;
 }
--- src/adapters/repositories/billingGroupingRepository.ts
+++ src/adapters/repositories/billingGroupingRepository.ts
@@ -8,6 +8,10 @@ import { ResponseTypes } from '@/helpers/interfaces/presenters/response.types';
 export class BillingGroupingRepository implements BillingGroupRepositoryContract<GroupBillingDatabaseForm> {
   constructor(private db: DBDriverContract<GroupBillingDatabaseForm, any>) {}
 
+  async all(): Promise<GroupBillingDatabaseForm[]> {
+    return await this.db.getAll();
+  }
+
   async create(entity: BillingGrouping): Promise<GroupBillingDatabaseForm> {
     const billingGroupingDatabaseForm = BillingGroupingPresenter.convertBillingGroupingEntityToDatabaseResponse(entity);
     const billing = await this.db.create(billingGroupingDatabaseForm, {
