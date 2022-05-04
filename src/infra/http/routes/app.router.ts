a2af2ccdac49723d81d10092aebde8aceeadfe05
OscarSilvaOfficial <OscarSilvaOfficial>
2022-05-04 19:24:40 -0300

    Atilizando presenter na controller

--- src/infra/http/routes/app.router.ts
+++ src/infra/http/routes/app.router.ts
@@ -1,6 +1,9 @@
+import { BillingGroupingPresenter } from '@/adapters/presenters/billingGrouping.presenter';
 import { BillingGroupingRepositoryFactory } from '@/adapters/repositories/factories/repositories.factory';
 import { GetAllBillingGroupingUseCase } from '@/core/useCases/getAllBillingGroupingUseCase';
-import { Controller, Get } from '@nestjs/common';
+import { GetAllBillingGroupingData } from '@/helpers/interfaces/useCases/getAllBillingGroupingData';
+
+import { Controller, Get, Query } from '@nestjs/common';
 
 @Controller()
 export class AppRouter {
@@ -12,7 +15,8 @@ export class AppRouter {
   }
 
   @Get('/groups')
-  async getAllBillingGroupings() {  
-    return await this.getAllBillingGroupingUseCase.execute(false);
+  async getAllBillingGroupings(@Query('billings') billings: boolean = false): Promise<GetAllBillingGroupingData[]> {  
+    const billingGroupings = await this.getAllBillingGroupingUseCase.execute({ withBillings: billings });
+    return BillingGroupingPresenter.serializeResponseAPI(billingGroupings);
   }
 }
